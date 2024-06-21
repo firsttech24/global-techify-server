c0b1f6f80c64adc59ecb670646e0aceb8de57b43
Shivam Prajapati <100841132+itsgeekhokage@users.noreply.github.com>
2024-06-21 18:10:23 +0530

    obl

--- routes/portal.js
+++ routes/portal.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { getPortalData, setPortalData } from "../controllers/portalController.js";
+import { getPortalData, sendContactMail, setPortalData } from "../controllers/portalController.js";
 
 const router = express.Router();
 
 router.get('/get', getPortalData);
 router.post('/set/:id', setPortalData);
+router.post('/mail', sendContactMail);
 
 export default router;
